2ba1c35090d006cf8535a69021f51801b657b654
Joel Bartlett <joel@murbar.com>
2019-02-04 22:36:51 -0500

    Commit compiled

--- build/main.js
+++ build/main.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello world');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector(`#${burger.dataset.target}`);
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-active');
+  nav.classList.toggle('is-active');
 });
