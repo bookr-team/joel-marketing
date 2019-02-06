b014f5177406ad6a596e4e3e6c6813790652d782
Joel Bartlett <joel@murbar.com>
2019-02-06 13:30:25 -0500

    Commit build files

--- build/main.js
+++ build/main.js
@@ -7,13 +7,13 @@ burger.addEventListener('click', () => {
   navMenu.classList.toggle('is-active');
 });
 
-// TODO init highlighter at first hover element with positionInital()
+// TODO init highlighter at first hover element with positionInitial()
 class Highlighter {
   constructor(config) {
     this.boundary = document.querySelector(config.boundarySelector);
     this.targets = document.querySelectorAll(config.targetsSelector);
     this.highlighterHeight = config.heightPx || 'cover';
-    this.highlighterOffset = config.offset || 0;
+    this.highlighterOffset = config.offsetPx || 0;
     this.highlighterColor = config.color || 'rgba(255, 165, 0, 0.5)';
     this.transitionMs = config.transitionMs || 200;
     this.windowMinWidth = config.windowMinWidth || 0;
@@ -33,14 +33,15 @@ class Highlighter {
   initHighlighter() {
     const highlighter = document.createElement('div');
     const styles = `
-      transition: all ${this.transitionMs}ms;
-      background: ${this.highlighterColor};
-      position: absolute;
-      z-index: 1000;
-      top: 0;
-      left: 0;
-      pointer-events: none;
-    `;
+        transition: all ${this.transitionMs}ms;
+        background: ${this.highlighterColor};
+        position: absolute;
+        z-index: 1000;
+        top: 0;
+        left: 0;
+        pointer-events: none;
+      `;
+
     highlighter.style.cssText = styles;
     document.body.append(highlighter);
     this.highlighter = highlighter;
@@ -77,6 +78,79 @@ const hl = new Highlighter({
   boundarySelector: 'nav .hover-effect-bound',
   targetsSelector: 'nav .navbar-menu a:not(.button)',
   heightPx: 4,
+  offsetPx: 20,
   color: '#fc5e32',
   windowMinWidth: 700
+});
+
+class AutomaticSlideShow {
+  constructor(config) {
+    this.config = config;
+    this.currentIndex = 1;
+    this.element = document.querySelector(config.componentElSelector);
+    this.slidesContainer = this.element.querySelector(config.slidesContainer);
+    this.sliderWidth = this.slidesContainer.clientWidth;
+    this.slides = this.slidesContainer.children;
+    this.slideDelayMs = config.slideDelayMs || 5000;
+    this.transitionMs = config.transitionMs || 300;
+    this.attachListeners();
+    this.cloneFirstAndLast();
+    this.transitionSlide();
+    this.run();
+  }
+  run() {
+    this.interval = setInterval(() => this.showNext(), this.slideDelayMs);
+  }
+  attachListeners() {
+    this.slidesContainer.addEventListener('mouseenter', () => this.pause());
+    this.slidesContainer.addEventListener('mouseleave', () => this.resume());
+    this.slidesContainer.addEventListener('transitionend', () => this.completeTransition());
+    window.addEventListener('resize', () => this.reset());
+  }
+  cloneFirstAndLast() {
+    const first = this.slides[0].cloneNode(true);
+    const last = this.slides[this.slides.length - 1].cloneNode(true);
+    first.id = 'first-clone';
+    last.id = 'last-clone';
+    this.slidesContainer.prepend(last);
+    this.slidesContainer.append(first);
+    this.slides = this.slidesContainer.children;
+  }
+  transitionSlide() {
+    const transformPx = -this.sliderWidth * this.currentIndex;
+    this.slidesContainer.style.transform = `translateX(${transformPx}px)`;
+  }
+  showNext() {
+    if (this.currentIndex >= this.slides.length - 1) return;
+    this.slidesContainer.style.transition = `transform ${this.transitionMs}ms ease-in-out`;
+    this.currentIndex++;
+    this.transitionSlide();
+  }
+  completeTransition() {
+    const slideId = this.slides[this.currentIndex].id || null;
+    if (slideId === 'last-clone' || slideId === 'first-clone') {
+      const offset = slideId === 'last-clone' ? 2 : this.currentIndex;
+      this.slidesContainer.style.transition = '';
+      this.currentIndex = this.slides.length - offset;
+      this.transitionSlide();
+    }
+  }
+  pause() {
+    clearInterval(this.interval);
+    console.log('pause');
+  }
+  resume() {
+    this.run();
+    console.log('resume');
+  }
+  reset() {
+    this.sliderWidth = this.slidesContainer.clientWidth;
+    this.transitionSlide();
+  }
+}
+
+// slides are all direct children of slidesContainer
+const slider = new AutomaticSlideShow({
+  componentElSelector: '.testimonials-component',
+  slidesContainer: '.testimonials-container'
 });
